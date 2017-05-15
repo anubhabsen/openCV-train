3808b6bd33895bfb323e7d02849640a19936e36b
Anubhab Sen <anubhabsen@gmail.com>
2017-05-15 14:06:02 +0530

    initial commit

---
+++ captureimage.py
@@ -0,0 +1,22 @@
+import sys
+import cv2
+
+camera_port = 0
+
+camera = cv2.VideoCapture(camera_port)
+
+def get_image():
+
+	val, im = camera.read()
+	return im
+
+print("Ready to take image")
+
+camera_capture = get_image()
+file = "/Users/anubhabsen/Desktop/CVIT/image1.jpg"
+
+cv2.imwrite(file, camera_capture)
+
+print("Image taken and saved to file")
+
+del(camera)
